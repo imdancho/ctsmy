4da14666edcee8fef8df1c222a664a2318e60a47
imdancho <113933603+imdancho@users.noreply.github.com>
2024-12-01 21:23:15 +0500

    Delete CTSDerailmentAnalysis directory

--- CTSDerailmentAnalysis/backend/internal/db/models.go
+++
@@ -1,8 +0,0 @@
-package db
-
-type User struct {
-	Id       int    `gorm:"primaryKey"`
-	Email    string `gorm:"uniqueIndex;not null"`
-	Password string `gorm:"not null"`
-	Role     string `gorm:"default:user;not null"`
-}
